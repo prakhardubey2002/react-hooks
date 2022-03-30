b541680849252b73f7238cd9c320e528051fde59
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-03-30 22:06:38 +0530

    UseEffect Completed

--- src/components/Useeffect.js
+++ src/components/Useeffect.js
@@ -1,11 +1,41 @@
+import React, { useState, useEffect } from "react";
 const Useeffect = () => {
-    return ( 
-        <>
-        <div className="hooks2">
-            
-        </div>
-        </>
-     );
+//   const [resourceType, setResourceType] = useState("posts");
+//   const [items, setItems] = useState([]);
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+//       .then((response) => response.json())
+//       .then((json) => setItems(json),console.log(items));
+//   }, [resourceType]); //[] empthy array doesnt change so it will execute only once even if dom changes but if offer specific value like here resourceType it will only execute when value specifed value is changes not everytim e application is rendered
+
+const [windowWidth,setWindowWidth]=useState(window.innerWidth);
+const handleResize = () =>{
+    setWindowWidth(window.innerWidth)
 }
- 
+useEffect(()=>{
+    window.addEventListener('resize',handleResize)
+
+    return ()=>{
+        window.removeEventListener('resize',handleResize)//cleanup as website will eventuall slowdown because of  events being accumulated
+    }
+},[])
+  return (
+    <>
+    <div className="wid">
+        {windowWidth}
+    </div>
+      {/* <div className="hooks2">
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item) => {
+          
+        return <p key={item.id} >{JSON.stringify(item) }</p>;
+      })} */}
+    </>
+  );
+};
+
 export default Useeffect;
